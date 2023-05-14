f58d44765baddee8467c1c369e0027b3564c5a22
JoseluisPR <joseluis.paris@constellaintelligence.com>
2023-05-14 16:55:44 +0200

    Adds keymaps

--- mappings.lua
+++ mappings.lua
@@ -18,11 +18,46 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+   
+    -- Save / Close file
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, 
+    ["<C-w>"] = { ":close<cr>" }, 
+    ["<C-q>"] = { ":q<cr>" }, 
+
+    -- Do not copy on register when pressing x
+    ["x"] = { "_x" }, 
+
+    -- Split window
+    ["sv"] = { "<C-w>v" }, 
+    ["sh"] = { "<C-w>s" }, 
+    ["se"] = { "<C-w>=" }, 
+
+    -- Tabs
+    ["<C-n>"] = { ":tabnew<cr>" }, 
+    ["tx"] = { ":tabclose<cr>" }, 
+    ["tn"] = { ":tabn<cr>" }, 
+    ["tp"] = { ":tabp<cr>" }, 
+
+    -- Quit search highlight until next search
+    ["<S-h>"] = { ":noh<cr>" },
+
+    -- Telescope
+    ["<C-p>"] = { "<cmd>Telescope find_files<cr>" },
+    ["<C-b>"] = { "<cmd>Telescope buffers<cr>" },
+    ["<C-f>"] = { "<cmd>Telescope live_grep<cr>" },
+    ["<C-h>"] = { "<cmd>Telescope help_tags<cr>" },
+    ["<C-g>"] = { "<cmd>Telescope grep_string<cr>" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
+  v = { 
+    -- Do not copy on register when pressing x
+    ["x"] = { "_x" }, 
+    
+    -- Maintain tab after using << or >>
+    ["<"] = { "<gv" },
+    [">"] = { ">gv" },
+  }
 }
