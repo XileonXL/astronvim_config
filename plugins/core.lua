58c33478163790989fce519c667e42c4808aeeb8
xfear <joseluis.alfaix@gmail.com>
2023-05-14 19:41:45 +0200

    Disables alpha-nvim

--- plugins/core.lua
+++ plugins/core.lua
@@ -20,6 +20,8 @@ return {
       return opts
     end,
   },
+
+  { "goolord/alpha-nvim", enabled = false },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
